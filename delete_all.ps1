2cfd4872cbbcbfbcdf532baba6308dd98cd99373
Vixine <3858343+Vixine@users.noreply.github.com>
2020-03-04 20:44:00 -0600

    added ps and refresh env

--- delete_all.ps1
+++ delete_all.ps1
@@ -1,3 +1,14 @@
-$folder = $Env:USERPROFILE + "\Desktop\*"
-Remove-Item -Path $folder -Include *.lnk, *.rdp
+$folder = [Environment]::GetFolderPath("Desktop") + "\*"
+Remove-Item -Path $folder -Include *.lnk, *.rdp, *.qds
+Remove-Item -Path "$($folder)PCC.url"
+$code = @'
+[System.Runtime.InteropServices.DllImport("Shell32.dll")] 
+private static extern int SHChangeNotify(int eventId, int flags, IntPtr item1, IntPtr item2);
 
+public static void Refresh()  {
+    SHChangeNotify(0x8000000, 0x1000, IntPtr.Zero, IntPtr.Zero);    
+}
+'@
+
+Add-Type -MemberDefinition $code -Namespace WinAPI -Name Explorer 
+[WinAPI.Explorer]::Refresh()
